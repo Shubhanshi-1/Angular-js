9ed776800749ca1c8c02d4874e5e61729cce1db5
Shubhanshi <vermashashi3421@gmail.com>
2025-03-06 13:29:04 +0530

    [Shubhanshi]Add. Displaying Bridgelabz logo using Property binding

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -9,5 +9,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
  // title = 'Helloworld';
-  message: string = "Hello from BridgeLabz"; 
+ message: string = "Hello from BridgeLabz"; 
+  imgUrl: string = "../assets/BL_logo_square_jpg.jpg"; 
 }
